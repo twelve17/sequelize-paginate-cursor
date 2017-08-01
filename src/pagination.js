2c1fedf916f021d5500c05a742f4e7921de2bc64
twelve17 <spam@twelve17.com>
2017-08-01 15:49:26 +0100

    WIP

--- src/pagination.js
+++ src/pagination.js
@@ -2,44 +2,122 @@
  * Created by mirabalj on 10/12/16.
  */
 
+const set = require("lodash.set");
+
+const orderHasId = (order, keyPaginated) =>
+  order.filter(inner => inner[0].toLowerCase() === keyPaginated) > 0;
+
+const isIdLastInOrder = (order, keyPaginated) =>
+  (order.map(inner => inner[0]).indexOf(keyPaginated) + 1) === order.length;
+
+const reverseOrderClause = (order) =>
+  order.map(([col, direction]) => [
+      col,
+      direction.toUpperCase() === "ASC" ? "DESC" : "ASC"
+  ]);
+
+const hashOrder = (order) => {
+  return order.reduce((acc, [col, dir]) => {
+   acc[col] = dir;
+   return acc;
+  });
+};
+
+const rowValuefy ({ where, orderHash }) => {
+  Object.keys(where).reduce((acc, k) => {
+
+    const orderColDir = orderHash[k] || 'ASC';
+    const sOrderColDir = orderColDir === 'ASC' ? "$gt" : "$lt";
+
+    let val = where[k];
+    if (typeof val === "object") {
+      val = rowValuefy(val);
+    } else {
+      const and = set(and, '$and.$or');
+
+      val = { $and: {
+
+
+      }
+
+
+
+    }
+
+  });
+
+};
+
 export default (Model, { name } = {}) => {
   // Pagination using cursor is here!
   const results = async function any({
     sinceId, maxId, limit = 1,
     select, where = {},
-    keyPaginated = 'id', reverse = false,
+    reverse = false,
+    order = [], keyPaginated = 'id',
     include = [], subQuery = true,
   } = {}, callback) {
     try {
+      /*
       const lsThanE = reverse ? '$gte' : '$lte';
       const lsThan = reverse ? '$gt' : '$lt';
       const gsThan = reverse ? '$lt' : '$gt';
       const findObject = where;
       const findCursor = {};
+      console.log("sinceId!", sinceId);
+      */
+
+
+      /*
+       * if cursor PK:
+       * find item, then for each column:
+       * -
+       *
+       *
+       */
 
       if (sinceId) {
-        findCursor[lsThanE] = sinceId;
-        findObject[keyPaginated] = findCursor;
+        const comparator = (reverse) ? '$lt' : '$gt';
+        const andVal = {};
+        andVal[keyPaginated] = {};
+        andVal[keyPaginated][comparator] = sinceId;
+        where = { $and: andVal };
+        //findCursor[lsThan] = sinceId;
+        //findObject[keyPaginated] = findCursor;
       }
 
+      /*
       if (maxId) {
         findCursor[gsThan] = maxId;
         findObject[keyPaginated] = findCursor;
       }
+      */
 
-      // Assign order of search
-      const order = keyPaginated + (reverse ? '' : ' DESC');
+      console.log("where", order);
+
+      // Assign order of search.  PK must be last.
+      if (!orderHasId(order, keyPaginated)) {
+        order.push([keyPaginated, 'ASC']);
+      } else if (!isIdLastInOrder(order, keyPaginated)) {
+        throw new Error('id must be the last sort order column');
+      }
+      const queryOrder = reverse ? reverseOrderClause(order) : order;
+      console.log("queryOrder", queryOrder, "sinceId", sinceId, "reverse", reverse);
+
+      //const keyPaginatedOrder = reverse ? 'ASC' : 'DESC';
+      //order.push([keyPaginated, keyPaginatedOrder]);
 
       // Execute query with limit
-      const objects = await this.findAll({
+      const objects = (!sinceId && reverse) ? [] : await this.findAll({
         where,
         subQuery,
         include,
         limit,
         attributes: select,
-        order,
+        order: queryOrder,
       });
 
+      /*
       let nextCursor = undefined;
       const len = objects.length;
 
@@ -62,11 +140,14 @@ export default (Model, { name } = {}) => {
           nextCursor = nextObject[keyPaginated];
         }
       }
+      */
 
       // Create paginate object
       const objectReturn = {
-        objects,
-        nextCursor,
+        objects: reverse ? objects.reverse() : objects,
+        order, // note we report the order requested, no the queryOrder we needed to use to get the proper records
+        reverse,
+        limit
       };
 
       // Call back, ¿exist?
